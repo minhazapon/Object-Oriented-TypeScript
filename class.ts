18f8bde9a08c79520c0870c955ad618f5aae9718
minhazapon <minhajulabedinapon@gmail.com>
2024-11-04 05:43:01 -0800

    added class file

--- class.ts
+++ class.ts
@@ -1,9 +1,29 @@
 {
 
+   class Animal {
 
+       constructor( public name: string, public food: string, public sound: string ){
+        }
 
-
-
-
+        RunBaby(){
     
+          console.log(`ei shalar${this.name} ja door mar ${this.food} kheye ne`)
+
+        }
+
+   }
+
+
+   const dog = new Animal("neri Kiutta", "aborjona", " ghau ghau ghewoooooo") 
+   const cat = new Animal("kalo biral", "fish har", "meeeewwwww" ) 
+
+//    console.log(dog)
+//    console.log(cat)
+   
+  cat.RunBaby();
+
+
+
+
+
 }
