68a88e2c7358564cf0ba15943c6ddcbd52b4dd3b
minhazapon <minhajulabedinapon@gmail.com>
2024-11-04 07:46:03 -0800

    added file

---
+++ TypeGurds3.ts
@@ -0,0 +1,74 @@
+{
+
+
+    
+    type allGurds = string | number
+
+
+    const gurds = (a: allGurds, b: allGurds): allGurds =>{
+
+         if(typeof a === "number" &&  typeof b === "number" ){
+             return a + b
+         } else {
+            return a.toString() + b.toString
+         }
+
+    }
+
+    const Gruds = gurds(22, 33) 
+
+    console.log(Gruds)
+
+
+
+    // ---------------------------------------------------------
+
+
+
+    type NormalUser = {
+
+         name: string
+          role: "admin vai"
+
+    }
+
+    type AdminUser = {
+
+         name: string,
+      
+
+    }
+
+
+    const UserZ = (user: NormalUser | AdminUser) =>{
+
+           if('role' in user){
+               console.log(`my ${user.name} is ${user.role}`)
+           } else {
+              console.log(`my ${user.name} `)
+           }
+    }
+
+    
+    const normalU: NormalUser = {
+        name: 'apon vai',
+        role: 'admin vai'
+    }
+
+    const adUser: AdminUser = {
+        name: 'apon vai'
+    }
+    UserZ(normalU)
+
+
+   
+
+
+
+
+
+
+
+
+    
+}
