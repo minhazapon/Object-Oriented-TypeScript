457545d3481aefc674b0e595732580ea0ef51516
minhazapon <minhajulabedinapon@gmail.com>
2024-11-04 21:13:20 -0800

    added abstract file

---
+++ Abstraction9.ts
@@ -0,0 +1,61 @@
+{
+
+
+
+    abstract class read {
+
+        constructor(public name: string){
+        }
+
+
+        disPlay() : string {
+
+           return "hello JavaScript"
+             
+        }
+
+        abstract find(name: string):any;
+   
+    }
+
+
+    class Students extends read {
+
+        sId: number;
+        constructor(name: string, sId: number){
+            super(name)
+            this.sId = sId
+ 
+        }
+        find(name: string) {
+            return `student name is ${name} `
+        }
+
+
+    }
+
+
+    let s1 = new Students('good student', 1999)
+    console.log(s1.sId)
+
+
+    console.log(s1.find('mark'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
