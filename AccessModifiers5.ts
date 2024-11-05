e812ea0c76f768339f916816a8350b56f58e3ad8
minhazapon <minhajulabedinapon@gmail.com>
2024-11-04 19:43:48 -0800

    added getter setter file

--- AccessModifiers5.ts
+++ AccessModifiers5.ts
@@ -18,9 +18,6 @@
     const returant1 = new FoodName("biriryani", "beefKaba", 'chicken crim couf')
     const returant2 = new FoodName("chicken kabab", "beef and khecuri", 'mashroom crim souf')
     
-
-    
-
     console.log(returant1)
     console.log(returant2)
 
---
+++ getterAndSetter6.ts
@@ -0,0 +1,60 @@
+{
+
+
+
+     
+    class FoodName {
+
+         
+        constructor( private Food1: string, public food2: string, public food3: number ) {}
+
+        // foodZ(){
+
+        //   console.log(`we are love ${this.Food1} ${this.food2}, ${this.food3}`)
+
+        // } 
+
+        get foodZ(){
+
+             return this.food2   
+        }
+
+        set Acount(amount: number){
+
+             this.food3 + amount
+
+            //  console.log(`${this.Acount}`)
+
+        }
+
+   }
+
+
+   const returant1 = new FoodName("biriryani", "beefKaba", 20)
+   const returant2 = new FoodName("chicken kabab", "beef and khecuri", 40)
+   
+
+   const f2 = returant1.foodZ
+   console.log(f2)
+
+  returant2.Acount=50      
+
+  const a = returant2.Acount
+  console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
